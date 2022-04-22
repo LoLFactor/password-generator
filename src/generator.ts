558c2d8688bbbebb64f5e3d1f620defb7b01ecb2
Vlad Pascu <pascu.vlad@gmail.com>
2022-04-23 00:38:31 +0300

    Document PasswordGenerator class

--- src/generator.ts
+++ src/generator.ts
@@ -6,17 +6,47 @@ const NUMERIC = '0123456789';
 const SYMBOLS = '~!@#$%^&*()-=[];\'\\,./_+{}:"|<>?';
 
 class PasswordGenerator {
+  /**
+   * Default alphabets used byt the password generator.
+   * Includes lowercase alpha characters, uppercase alpha characters, numbers and symbols.
+   */
   public static readonly DEFAULT_ALPHABETS = [LOWERCASE_ALPHA, UPPERCASE_ALPHA, NUMERIC, SYMBOLS];
 
+  /**
+   * Main class of the package.
+   * Generates passwords according to the supplied parameters (or default ones if not specified) and using the supplied
+   * RNG class (or a default one is not specified).
+   *
+   * @param alphabets Array of strings comprising the alphabets from which to pick characters for password generation. Defaults to {@link PasswordGenerator.DEFAULT_ALPHABETS}.
+   * @param rng RNG class to be used for password generation. Defaults to a default {@link RNG} class using a JS Math implementation.
+   *
+   * @see PasswordGenerator.DEFAULT_ALPHABETS
+   * @see RNG
+   */
   constructor(
     protected alphabets: string[] = PasswordGenerator.DEFAULT_ALPHABETS,
     protected rng = new RNG(),
   ) {}
 
+  /**
+   * Retrieves the number of alphabets used by this password generator.
+   * Useful when creating custom distributions.
+   *
+   * @see RNG.generateDistribution
+   */
   public getAlphabetCount(): number {
     return this.alphabets.length;
   }
 
+  /**
+   * The main method of the class.
+   * Generates a password of the specified length and using the supplied distribution (or a default one if not specified).
+   *
+   * @param length The length of the generated password. Defaults to 16.
+   * @param distribution An array of integers which control how many characters from each alphabet are picked. Defaults to a completely random distribution that does not guarantee the presence of all alphabets.
+   *
+   * @see RNG.generateDistribution
+   */
   public generate(
     length: number = 16,
     distribution: number[] = this.rng.generateDistribution(length, this.getAlphabetCount()),
